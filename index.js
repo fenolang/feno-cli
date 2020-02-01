8a2fe460a3c5939a8b269df9b9799ff8ee9867c5
YahirGNz <dakdaker55@gmail.com>
2020-01-31 22:43:33 -0700

    feat: Create projects!
    v: 1.0.0-beta

--- index.js
+++ index.js
@@ -1,16 +1,59 @@
 const chalk = require('chalk');
 const clear = require('clear');
-const figlet = require('figlet');
+const path = require('path');
+const center = require('center-align');
 
 const files = require('./lib/files');
 const inquirer = require('./lib/inquirer');
+const argv = require('minimist')(process.argv.slice(2));
+
+var options = {
+    fit: 'box',
+    width: 15,
+    height: 15
+}
+
+const logo = `
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMM++++++++++++NMsh+++++++++++mMM
+MMMMMMMM           MMMs           MMMM
+MMMMMMMM    MMMMMMMMMMs     MMMMMMMMMM
+MMMMMMMM    MMMMMMMMMMs     MMMMMMMMMM
+MMMMMMMM           MMMs           MMMM
+MMMMMMMM    sssssssMMMs     MMMMMMMMMM
+MMMMMMMM    MMMMMMMMMMs     MMMMMMMMMM
+MMMMMMMM    MMMMMMMMMMs     NNNNNNNMMM
+MMMMMMMM    MMMMMMMMMMs            MMM
+MMMMMMMMhssshMMMMMMMMMMdsssssssssssmMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+`
 
 clear();
-console.log(chalk.green(figlet.textSync('N U E',{horizontalLayout:'full'})))
 
 const run = async () => {
-    const credentials = await inquirer.askGithubCredentials();
-    console.log(credentials);
+    let order = argv._[0];
+    if (order == 'create-new') {
+        console.log(logo)
+        console.log("Welcome to", chalk.green.bold("Feno CLI v1.0.0-beta"), "!")
+        console.log("Prepare your new project:");
+        const credentials = await inquirer.askGithubCredentials();
+        await files.createProject(credentials);
+        clear();
+        console.log("\u{1F98A}  Creating project in", path.join(process.cwd(), `/${credentials.projectname}`));
+        console.log(`\u{1F389}  Successfully created project ${chalk.yellow(credentials.projectname)}`);
+        console.log(`\u{1F449}  Get started with the following commands:`)
+        console.log(`\n$ ${chalk.green(`cd ${credentials.projectname}`)}`)
+        console.log(`$ ${chalk.green(`npm i`)}`);
+        console.log(`$ ${chalk.green(`npm run dev`)}`);
+        console.log(`\n${chalk.green.bold("Create something amazing!\n\n")}`);
+    } else {
+
+    }
 }
 
 run();
---
+++ lib/README.md
@@ -0,0 +1,32 @@
+## About Feno
+
+Feno is a programming language transpiled to JavaScript to improve your productivity to the maximum when developing a website. With Feno you can increase the interactivity of your site to the fullest in addition to slightly modifying the HTML markup to make it more readable and understandable to the first. Feno has a much simpler and more powerful syntax to program EVERYTHING you imagine.
+
+## Learning Feno
+
+You can learn everything about Feno with his own [documentation](https://fenolang.heroku.com/docs) and start working in a project.
+
+## License
+
+The Feno programming language is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
+
+## Start now
+
+So you had Feno installed now uh? First, you need to install the necessary dependencies to work, just type the following code in your console:
+```
+$ npm i
+```
+
+Later just create a "Hello World" in your `index.feno` located in `pages/` folder:
+
+```html
+doc {
+    <p>Hello, World!</p>
+}
+```
+
+Strange? Interesting? Yeah! But this is just a fairly basic start, now you can go to the official documentation, learn how Feno works and create your next project like never before ;))!
+
+See you and thank you very much!
+
+\- YahirGNz (Creator)
